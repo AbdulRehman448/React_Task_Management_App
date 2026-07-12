63370f4b2b2be551a919cf71f271b9eeb3daa000
AbdulRehman448 <abdulrehman.tp.786@gmail.com>
2026-07-12 13:53:58 +0500

    Refactor App component to implement routing with React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import About from "./pages/About/About";
+import NotFound from "./pages/NotFound/NotFound";
+
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-900">
-      <h1 className="text-5xl font-bold text-white">
-        React Task Management App 🚀
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/register" element={<Register />} />
+
+        <Route path="/dashboard" element={<Dashboard />} />
+
+        <Route path="/about" element={<About />} />
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
