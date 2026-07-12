2580ca25d2d7a0330d96b14abcd478dbcd349a1d
AbdulRehman448 <abdulrehman.tp.786@gmail.com>
2026-07-12 15:47:51 +0500

    Add Footer component for app branding and copyright information

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white text-center py-4">
+      <p>© 2026 React Task Management App. All Rights Reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
